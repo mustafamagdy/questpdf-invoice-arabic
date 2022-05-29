b93d8f1c97541717e74e21744cbd1a101e512622
MMagdy <{ID}+{username}@users.noreply.github.com>
2022-05-29 18:42:34 +0300

    wip pdf generator

--- InvoiceDocumentDataSource.cs
+++ InvoiceDocumentDataSource.cs
@@ -4,52 +4,53 @@ using QuestPDF.Helpers;
 
 namespace QuestPDF.ExampleInvoice
 {
-    public static class InvoiceDocumentDataSource
+  public static class InvoiceDocumentDataSource
+  {
+    private static Random Random = new Random();
+
+    public static InvoiceModel GetInvoiceDetails()
     {
-        private static Random Random = new Random();
-        
-        public static InvoiceModel GetInvoiceDetails()
-        {
-            var items = Enumerable
-                .Range(1, 25)
-                .Select(i => GenerateRandomOrderItem())
-                .ToList();
-            
-            return new InvoiceModel
-            {
-                InvoiceNumber = Random.Next(1_000, 10_000),
-                IssueDate = DateTime.Now,
-                DueDate = DateTime.Now + TimeSpan.FromDays(14),
+      var items = Enumerable
+        .Range(1, 25)
+        .Select(i => GenerateRandomOrderItem())
+        .ToList();
 
-                SellerAddress = GenerateRandomAddress(),
-                CustomerAddress = GenerateRandomAddress(),
-                
-                Items = items,
-                Comments = Placeholders.Paragraph()
-            };
-        }
+      return new InvoiceModel
+      {
+        InvoiceNumber = Random.Next(1_000, 10_000),
+        IssueDate = DateTime.Now,
+        DueDate = DateTime.Now + TimeSpan.FromDays(14),
 
-        private static OrderItem GenerateRandomOrderItem()
-        {
-            return new OrderItem
-            {
-                Name = Placeholders.Label(),
-                Price = (decimal) Math.Round(Random.NextDouble() * 100, 2),
-                Quantity = Random.Next(1, 10)
-            };
-        }
+        SellerAddress = GenerateRandomAddress(),
+        CustomerAddress = GenerateRandomAddress(),
 
-        private static Address GenerateRandomAddress()
-        {
-            return new Address
-            {
-                CompanyName = Placeholders.Name(),
-                Street = Placeholders.Label(),
-                City = Placeholders.Label(),
-                State = Placeholders.Label(),
-                Email = Placeholders.Email(),
-                Phone = Placeholders.PhoneNumber() 
-            };
-        }
+        Items = items,
+        Comments = Placeholders.Paragraph()
+      };
     }
+
+    private static OrderItem GenerateRandomOrderItem()
+    {
+      return new OrderItem
+      {
+        // Name = Placeholders.Label(),
+        Name = "اسم الصنف الطويل  - هنا مكانه".ArabicWithFontGlyphsToPfd(),
+        Price = (decimal)Math.Round(Random.NextDouble() * 100, 2),
+        Quantity = Random.Next(1, 10)
+      };
+    }
+
+    private static Address GenerateRandomAddress()
+    {
+      return new Address
+      {
+        CompanyName = Placeholders.Name(),
+        Street = Placeholders.Label(),
+        City = Placeholders.Label(),
+        State = Placeholders.Label(),
+        Email = Placeholders.Email(),
+        Phone = Placeholders.PhoneNumber()
+      };
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -6,29 +6,32 @@ using QuestPDF.Fluent;
 
 namespace QuestPDF.ExampleInvoice
 {
-    class Program
+  class Program
+  {
+    /// <summary>
+    /// For documentation and implementation details, please visit:
+    /// https://www.questpdf.com/documentation/getting-started.html
+    /// </summary>
+    static void Main(string[] args)
     {
-        /// <summary>
-        /// For documentation and implementation details, please visit:
-        /// https://www.questpdf.com/documentation/getting-started.html
-        /// </summary>
-        static void Main(string[] args)
-        {
-            var filePath = "invoice.pdf";
-            
-            var model = InvoiceDocumentDataSource.GetInvoiceDetails();
-            var document = new InvoiceDocument(model);
-            document.GeneratePdf(filePath);
+      var r = new Random();
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                Process.Start("explorer.exe", filePath);
-            }
-            else
-            {
-                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), filePath);
-                Console.WriteLine($"Output PDF file is available here: {fullPath}");
-            }
-        }
+      var filePath = $"invoice{r.Next(0, 1000)}.pdf";
+
+      var model = InvoiceDocumentDataSource.GetInvoiceDetails();
+      var document = new InvoiceDocument(model);
+      document.GeneratePdf(filePath);
+
+      if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+      {
+        Process.Start("explorer.exe", filePath);
+      }
+      else
+      {
+        var fullPath = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+        Process.Start("open", fullPath);
+        Console.WriteLine($"Output PDF file is available here: {fullPath}");
+      }
     }
+  }
 }
